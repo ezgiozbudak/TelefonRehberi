4355a34eb7d25b25be94a94f1ad56ed4b6d0fc43
ezgiozbudak <96118303+ezgiozbudak@users.noreply.github.com>
2022-04-10 23:44:35 +0300

    Add files via upload

--- TelefonRehberiUygulamasi/TelefonRehberiUygulamasi/NumaraSil.cs
+++ TelefonRehberiUygulamasi/TelefonRehberiUygulamasi/NumaraSil.cs
@@ -6,10 +6,35 @@ namespace TelefonRehberiUygulamasi
 {
     public class NumaraSil : Kisiler
     {
-        public void ListedeAra(string Isim ,string Soyisim)
+        public int ListedeAra(string Isim ,string Soyisim)
         {
-           
+            int kontrol = 0;
+            int index = -1;
+            foreach (var item in KisiListesi)
+            {
+                if (item.isim == Isim && item.soyisim == Soyisim)
+                {
+                   index = KisiListesi.IndexOf(item);
+                   Console.WriteLine("Kisi:"+ "\nİsim :" + item.isim + "\nSoyisim:" + item.soyisim + "\nNumara:" + item.numara);
+                   kontrol++;
+                }
+                else
+                {
+
+                }
+            }
+            return index;
         }
 
+        public void Sil(int sayi)
+        {
+            if(sayi > 0 )
+               KisiListesi.RemoveAt(sayi);
+            else if(sayi<0 || sayi > KisiListesi.Count)
+            {
+                Console.WriteLine("Kişi Bulunamadı.");
+            }
+               
+        }
     }
 }
